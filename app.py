258eb5523594f9d0f0278944230af196b2bcdd80
Dev Raj Singh <55313450+devraj4522@users.noreply.github.com>
2023-05-24 21:25:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from fer import FER
 import cv2
 import time
 from flask import Flask, request, jsonify
+import snscrape.modules.twitter as sntwitter
+
 
 app = Flask(__name__)
 emotion_detector = FER(mtcnn=True)
@@ -31,6 +33,54 @@ def predict_image(img):
 def index():
     res = {"app": "Text sentiment analysis for social media"}
     return jsonify(res)
+
+def fetch_user_tweets(user):
+  maxTweets = 500
+
+  # Creating list to append tweet data to
+  tweets_list = []
+
+  # Using TwitterSearchScraper to scrape data and append tweets to list
+  for i,tweet in enumerate(sntwitter.TwitterProfileScraper(f'{user}').get_items()):
+      if i>maxTweets:
+          break
+      media = []
+      if tweet.media:
+        for med in tweet.media:
+          media.append({"media_url": med})
+      curr_dict = {"media": media, "user": tweet.user.username, "id": tweet.id, "text": tweet.content, "date": tweet.date.strftime("%Y-%m-%d %H:%M:%S"), "tweet": tweet}
+      tweets_list.append(curr_dict)
+  return tweets_list
+
+
+def fetch_tweets_from_id(tweet_id):
+  maxTweets = 500
+
+  # Creating list to append tweet data to
+  tweets_list = []
+
+  # Using TwitterSearchScraper to scrape data and append tweets to list
+  for i,tweet in enumerate(sntwitter.TwitterTweetScraper(tweet_id).get_items()):
+      if i>maxTweets:
+          break
+      media = []
+      if tweet.media:
+        for med in tweet.media:
+          media.append({"media_url": med})
+      curr_dict = {"media": media, "user": tweet.user.username, "id": tweet.id, "text": tweet.content, "date": tweet.date.strftime("%Y-%m-%d %H:%M:%S"), "tweet": tweet}
+      tweets_list.append(curr_dict)
+  return tweets_list
+
+
+@app.route('/fetch_user_tweets', methods=['GET'])
+def fetch_user_tweets_route():
+    user = request.args.get('user')
+    users = fetch_user_tweets(user)
+    return jsonify(fetch_user_tweets(user))
+
+@app.route('/fetch_tweets_from_id', methods=['GET'])
+def fetch_tweets_from_id_route():
+    return jsonify(fetch_tweets_from_id(request.args.get('tweet_id')))
 
 @app.route('/predict', methods=['POST'])
 def predict():
